76703f47cfe30b306762921b9fcc227e2723eb89
Sebastien <seb75@fedoraproject.org>
2019-11-07 11:54:20 +0100

    created schema

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    "name": {
+        type: String,
+        required: true
+    },
+    "lastname": {
+        type: String,
+        required: true
+    },
+    "birth": {
+        type: String,
+        required: true
+    },
+    "passion": {
+        type: String,
+    }
+});
+
+module.exports = user = mongoose.model('user', UserSchema);
