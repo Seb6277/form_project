b5054869cd18c1e782b9a88e49ae30120be3b15b
Sebastien <seb75@fedoraproject.org>
2019-11-07 13:32:20 +0100

    finished chap 2

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+// User model
+const User = require('../models/User');
+
 router.get("/get_users", (req, res) => {
-    res.json({
-        users: {
-            name: 'john',
-            lastname: 'doe',
-            birth: '24/01/89',
-            passion: 'Marilyn Monroe'
+    User.find(function (err, items) {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.send(items);
+            res.end();
         }
     });
+});
+
+router.post("/register", function (req, res) {
+    if ((req.body.src !== null) && (req.body.header !== null)){
+        const newUser = new User({
+            "name": req.body.name,
+            "lastname": req.body.lastname,
+            "birthday": req.body.birthday,
+            "passion": req.body.passion
+        });
+        newUser.save().then((item) => {
+            res.status(201).json(item).end();
+        }).catch(error => {
+        console.log(error);
+            res.status(500).end();
+        });
+    }
 });
 
 module.exports = router;
