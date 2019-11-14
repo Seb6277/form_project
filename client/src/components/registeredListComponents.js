c97c98bb6d5d1bc844d1017e944916e3ec2a9762
Sebastien <seb75@fedoraproject.org>
2019-11-14 11:04:27 +0100

    finished form first part

--- client/src/components/registeredListComponents.js
+++ client/src/components/registeredListComponents.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class RegisteredList extends React.Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            userList: ['Lionel', 'Mickael']
+        }
+    }
+
+    generateList() {
+        const userList = this.state.userList;
+        userList.map((user) => {
+            return(
+                <p key={user}>
+                    {user}
+                </p>
+            )
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Liste des inscrits : </h2>
+                {this.state.userList.map((user) => <p key={user}>{user}</p>)}
             </div>
         );
     }
